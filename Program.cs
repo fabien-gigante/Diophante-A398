14fd4fc9861e59b981a2d5a9781eba42e1d9d75d
fabien-gigante <fabien.gigante@free.fr>
2023-04-28 14:09:55 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Maths;
+using System.Diagnostics;
+
+using number = System.Int64;
+
+new A398().Run();
+
+class A398 {
+  readonly Primes primes = new();
+
+  public void Run() {
+    Console.OutputEncoding = System.Text.Encoding.UTF8;
+    Stopwatch s = new(); s.Start();
+    foreach (number p in primes.All) {
+      number k = primes.Divisors(p - 1).Select(d => d + 1).Where(primes.IsPrime).Product();
+      Console.WriteLine($"p={p} k={k}");
+      if (k > 1e12) break;
+    }
+    s.Stop(); Console.WriteLine($"{s.ElapsedMilliseconds}ms elapsed");
+  }
+}
